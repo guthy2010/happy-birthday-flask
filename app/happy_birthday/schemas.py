c4ceba305e9e99ccbc9647f67edeb8245aa8baba
Bryce Bonvillain <bryce.bonvillain@deposit-solutions.com>
2020-01-28 16:49:13 +0100

    Moving logic to schemas and adding happy birthday functionality, also adding basic error handling to views

--- app/happy_birthday/schemas.py
+++ app/happy_birthday/schemas.py
@@ -1,12 +1,30 @@
 from marshmallow import Schema, fields, post_dump, pre_load, post_load, validates, ValidationError
 from app.database import db
 from app.happy_birthday.models import Users
-
+from datetime import datetime,timedelta
 
 #Schemas to map database relationships
 class UserDetailsSchema(Schema):
     username = fields.Str(required=True)
     dateOfBirth = fields.DateTime(required=True)
+
+
+    @validates('username')
+    def validate_username(self, username, **kwargs):
+        if not username.isalpha():
+            raise ValidationError(
+                '{username} contains characters that are not letters!'.format(username=username)
+            )
+        if bool(Users.query.filter_by(username=username).first()):
+            raise ValidationError(
+                '{username}" user already exists.'.format(username=username)
+            )
+
+
+    @pre_load
+    def clean_userdata(self,data, **kwargas):
+        data['dateOfBirth'] = datetime.strptime(data['dateOfBirth'], '%Y-%m-%d').isoformat()
+        return data
 
 
     @post_load
@@ -15,3 +33,17 @@ class UserDetailsSchema(Schema):
         db.session.add(user)
         db.session.commit()
         self.instance = user
+
+
+    @post_dump
+    def calculate_birthday(self,data,**kwargs):
+        date = datetime.strptime(data['dateOfBirth'], "%Y-%m-%dT%H:%M:%S")
+        today = datetime.now()
+        cleaned_date = date.replace(year=today.year)
+        birthday = abs(today - cleaned_date).days
+        if birthday > 0:
+            ret = 'Hello, {user}! Your birthday is in {birthday} day(s)'.format(user=data['username'], birthday=birthday)
+        else:
+            ret = 'Hello, {user}! Happy Birthday!)'.format(user=data['username'])
+        data['birthday_info'] = ret
+        return data
--- app/happy_birthday/views.py
+++ app/happy_birthday/views.py
@@ -1,9 +1,7 @@
 from http import HTTPStatus
-
 from flask import Blueprint, jsonify
 from flask import request
-from datetime import datetime
-
+from marshmallow import ValidationError
 from app.happy_birthday.models import Users
 from app.happy_birthday.schemas import UserDetailsSchema
 from app.database import db
@@ -12,19 +10,31 @@ from app.database import db
 happy_birthday = Blueprint('hello', __name__)
 
 # Add get method to route for specific searching or fetching all
-@happy_birthday.route('/', methods=["GET"])
+@happy_birthday.route('/', methods=['GET'])
+@happy_birthday.route('/<string:username>', methods = ['GET'], strict_slashes=False)
 def retrieve_username(username=None):
-    datacenters = Users.query.all()
-    schema = UserDetailsSchema(many=True).dump(datacenters)
+    if username:
+        try:
+            user = Users.query.filter_by(username=username).first_or_404()
+            schema = UserDetailsSchema().dump(user)
+            return jsonify(schema['birthday_info']), HTTPStatus.OK
+
+        except Exception:
+            return '{} not found!'.format(username), HTTPStatus.BAD_REQUEST
+
+        users = Users.query.all()
+        schema = UserDetailsSchema(many=True).dump(users)
+
     return jsonify(schema), HTTPStatus.OK
 
 
-@happy_birthday.route('/<string:username>', methods = ['POST'], strict_slashes=False)
+@happy_birthday.route('/<string:username>', methods = ['POST','PUT'], strict_slashes=False)
 def add_user(username=None):
     data = request.get_json()
     data['username'] = username
-    data['dateOfBirth'] = datetime.strptime(data['dateOfBirth'], '%Y-%m-%d').isoformat()
-    schema = UserDetailsSchema()
-    schema.load(data)
-    print(data)
+    try:
+        UserDetailsSchema().load(data)
+    except ValidationError as err:
+        return jsonify(err.messages), HTTPStatus.BAD_REQUEST
+
     return jsonify(data), HTTPStatus.CREATED
